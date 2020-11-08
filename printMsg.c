3cd49abdd81772f6a6595e2dd1296d7e4ee54c57
akshaygodse13 <72199103+akshaygodse13@users.noreply.github.com>
2020-11-08 23:35:29 +0530

    Update printMsg.c

--- printMsg.c
+++ printMsg.c
@@ -1,43 +1,67 @@
+#include "stdio.h"
 #include "stm32f4xx.h"
-#include<stdio.h>
-void printMsg(const int a)
+#include "string.h"
+void printMsg1(const int a,const int b,const int c,const int d)
 {
 	 char Msg[100];
 	 char *ptr;
-	 sprintf(Msg, "%x", a);
-	 ptr = Msg ;
-   while(*ptr != '\0'){
+	 if(c == 0 && d == 0)
+	 {
+	 if(b == 0)
+		 sprintf(Msg,"\n Logic Function : AND \n");
+	if(b == 1)
+		 sprintf(Msg,"\n Logic Function : OR \n");
+	if(b == 2)
+		 sprintf(Msg,"\n Logic Function : NOT \n");
+	if(b == 3)
+		 sprintf(Msg,"\n Logic Function : NAND \n");
+	if(b == 4)
+		 sprintf(Msg,"\n Logic Function : NOR \n");
+	ptr = Msg ;
+   while(*ptr != '\0')
+	 {
       ITM_SendChar(*ptr);
       ++ptr;
    }
-}
-void Printtruthtable(const int  a)
-{
-static char  Msg[100];
-static  int16_t i;	
-int j;	
-j=0;	
-char *ptr;	
-  Msg[0] = 1 ;
-	Msg[1] = 0 ;
-	Msg[2] = 0 ;
-	Msg[3] =(char)((a>>3)&1); 
-  Msg[4] = 1 ; 
-  Msg[5] = 0 ; 
-  Msg[6] = 1 ; 
-	Msg[7]=(char)((a>>2)&1);
-	Msg[8] = 1 ; 
-	Msg[9] = 1 ;
-  Msg[10]= 0 ;
-	Msg[11]=(char)((a>>1)&1);
-  Msg[12]= 1 ;
-  Msg[13]= 1 ;
-  Msg[14]= 1 ; 		
-	Msg[15]=(char)(a&1);
-	ptr=Msg;
-   while(j<=15){
+	 
+	sprintf(Msg," X0   X1   X2    Y \n");
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
       ITM_SendChar(*ptr);
       ++ptr;
-		 ++j;
+   }
+ }
+	 
+ sprintf(Msg, " %c",'1');
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 
+	 sprintf(Msg, "    %d", c);
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 
+	 sprintf(Msg, "    %d", d);
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+ 
+	 sprintf(Msg, "     %d\n", a);
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
    }
 }
